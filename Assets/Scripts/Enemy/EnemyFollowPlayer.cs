b8fac3f263bd743f9b7d2e33af3b0a9deb7bc568
hungtqclone <121530023+hungtqclone@users.noreply.github.com>
2023-10-17 22:23:19 +0700

    update Enemy Follow Player

---
+++ Assets/Scripts/Enemy/EnemyFollowPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    [SerializeField] protected Transform player;
+    [SerializeField] protected float moveSpeed = 5f;
+    [SerializeField] protected float stoppingDistance = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(player == null)
+        {
+            Debug.LogError("Bạn chưa khởi tạo player");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            Vector3 direction = player.position - transform.position;
+            direction.Normalize();
+
+            transform.Translate(direction * moveSpeed * Time.deltaTime);
+        }
+    }
+}
--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -1,18 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField] protected List<Transform> listEnemy;
+    [SerializeField] protected Transform holder;
     // Start is called before the first frame update
+
+    private void Reset()
+    {
+        LoadComponent();
+    }
     void Start()
     {
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            EnemySpawners();
+        }
+    }
+
+    protected virtual void LoadComponent()
+    {
+        ListEnemy();
+        holder = transform.Find("Holder");
+    }
+
+    protected virtual void ListEnemy()
+    {
+        Transform enemyPrefabs = transform.Find("Prefabs");
+        foreach (Transform prefabs in enemyPrefabs)
+        {
+            this.listEnemy.Add(prefabs);
+            prefabs.gameObject.SetActive(false);
+        }
+    }
+
+    protected virtual void ListEnemySpawner(int listNumber)
+    {
+        foreach (Transform prefabs in this.listEnemy)
+        {
+            if(prefabs == this.listEnemy[listNumber])
+            {
+                CreateEnemy(prefabs);
+            }
+        }
+    }
+
+    protected virtual void CreateEnemy(Transform enemy)
+    {
+        Transform enemySpawner = Instantiate(enemy, new Vector3(Random.Range(-8,8),Random.Range(-4,4),0), Quaternion.Euler(0, 0, 0));
+        enemySpawner.gameObject.SetActive(true);
+        enemySpawner.parent = this.holder;
+    }
+
+    protected virtual void EnemySpawners()
+    {
+        ListEnemySpawner(0);
     }
 }
