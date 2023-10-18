766df063630e341c16affe430174fa2b63419c09
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-18 13:28:16 +0700

    health and exp bar

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 4;
+    public int currentHealth;
+    public HealthBar healthBar;
+    public GameObject panelDead;
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHeallth(currentHealth);
+    }
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D collision)
+
+    {
+      
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+    }
+
+
+    private void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        healthBar.SetHealth(currentHealth);
+        if (currentHealth == healthBar.slider.minValue)
+        {
+            Debug.Log("DEAD");
+            Time.timeScale = 0f;
+            panelDead.SetActive(true);
+        }
+    }
+
+    public void Heal(int heal)
+    {
+        Debug.Log("=====================>HEAL");
+        currentHealth += heal;
+        healthBar.SetHealth(currentHealth);
+
+    }
+}
