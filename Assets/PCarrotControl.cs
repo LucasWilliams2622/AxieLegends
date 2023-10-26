7798624d8ca190619302a3e63da94e1b0667e0ad
southsafe0123 <naman.nguyen49002@gmail.com>
2023-10-26 14:12:47 +0700

    làm ơn đừng lỗi cà rốt nữa

--- Assets/PCarrotControl.cs
+++ Assets/PCarrotControl.cs
@@ -2,17 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PiecingCarrotController : MonoBehaviour
+public class PCarrotControl : MonoBehaviour
 {
-    
     [SerializeField] private GameObject carrot;
     [SerializeField] private GameObject targetToFollow;
-    // Start is called before the first frame update
 
-    // Update is called once per frame
     void Update()
     {
-        if(carrot.activeInHierarchy == false)
+        if (carrot.activeInHierarchy == false)
         {
             carrot.SetActive(true);
         }
