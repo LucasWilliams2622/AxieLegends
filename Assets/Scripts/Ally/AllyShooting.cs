34adaa64a6316edb3eeaf91a635161fd68c47419
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-26 11:36:22 +0700

    a

--- Assets/Scripts/Ally/AllyShooting.cs
+++ Assets/Scripts/Ally/AllyShooting.cs
@@ -4,7 +4,23 @@ using UnityEngine;
 
 public class AllyShooting : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform Gun;
+    public Vector2 direction;
+    public GameObject Bullet;
+    public float BulletSpeed;
+    public Transform ShootPoint;
+    public float fireRate;
+    float ReadyForNextShot;
+
+    [SerializeField] private AudioSource sniperSound;
+    [SerializeField] protected bool autoAttack;
+    [SerializeField] protected Vector2 mousePos;
+    [SerializeField] protected Vector2 mousePos2;
+    [SerializeField] protected float timeDelayAuto;
+    [SerializeField] protected Transform enemyNearThe;
+    [SerializeField] protected float mousePosX;
+    [SerializeField] protected float mousePosY;
+    [SerializeField] protected float timeDelayAttack;
     void Start()
     {
         
