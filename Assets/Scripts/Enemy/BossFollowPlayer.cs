84853ee3abe9719221ac4408b8d708eadafd5cf9
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-27 23:49:38 +0700

    a

--- Assets/Scripts/Enemy/BossFollowPlayer.cs
+++ Assets/Scripts/Enemy/BossFollowPlayer.cs
@@ -4,26 +4,35 @@ using UnityEngine;
 
 public class BossFollowPlayer : FollowToDistance
 {
-    [SerializeField] protected float speed;
+    [SerializeField] public float speed ;
 
+    public BossFinalAnimation bossFinalAnimation;
 
-    private void Awake()
+   
+    private void FixedUpdate()
     {
         speed = moveSpeed;
+       /* if (moveSpeed != 0)
+        {
+            bossFinalAnimation.PlayRun();
+        }*/
     }
- 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             moveSpeed = 0;
+            bossFinalAnimation.PlayAttack();
+               
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            moveSpeed = speed;
+            moveSpeed = 3;
+          
         }
     }
 
