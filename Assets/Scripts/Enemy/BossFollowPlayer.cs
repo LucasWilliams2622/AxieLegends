da52a04509d43f905a201239acfc67acf6d4f639
Lucas_Williams <nguyenvanson2622003@gmail.com>
2023-10-28 23:22:14 +0700

    fix flow game

--- Assets/Scripts/Enemy/BossFollowPlayer.cs
+++ Assets/Scripts/Enemy/BossFollowPlayer.cs
@@ -73,8 +73,6 @@ public class BossFollowPlayer : FollowToDistance
         base.Update();
     }
 
-
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
--- Assets/Scripts/Player/PlayerLevelBuff.cs
+++ Assets/Scripts/Player/PlayerLevelBuff.cs
@@ -32,7 +32,6 @@ public class PlayerLevelBuff : MonoBehaviour
 
     }
     
-   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("EXP"))
@@ -45,11 +44,11 @@ public class PlayerLevelBuff : MonoBehaviour
                 }
                 if (currentLevel == 2)
                 {
-                    CollectExp(6);
+                    CollectExp(5);
                 }
                 if (currentLevel == 3)
                 {
-                    CollectExp(5);
+                    CollectExp(4);
                 }
                 if (currentLevel == 4)
                 {
